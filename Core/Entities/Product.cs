35539efffda91b3f67a3621f7137a8308efdcbff
ellloy <webing1977@gmail.com>
2022-07-08 15:06:47 +0200

    first commit

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities;
+
+
+namespace Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ RestaurantAPI2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RestaurantAPI2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
